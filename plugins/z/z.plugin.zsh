b1a0945c69062bb628c60f0cc1fce3b351085549
mattmc3 <mattmc3@gmail.com>
2019-09-14 22:18:33 -0400

    Use XDG dirs for z

---
+++ plugins/z/z.plugin.zsh
@@ -0,0 +1,8 @@
+if [[ $USE_XDG_DIRS == true ]]; then
+  [[ -d "$XDG_DATA_HOME"/z ]] || mkdir -p "$XDG_DATA_HOME"/z
+  export _Z_DATA="$XDG_DATA_HOME"/z/data
+else
+  export _Z_DATA="$HOME"/.z
+fi
+
+source $ZSH/plugins/z/z.plugin.zsh
