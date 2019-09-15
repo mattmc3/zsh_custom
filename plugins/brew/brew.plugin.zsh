946ff9c50a645faa67621332153356b8e8dde87e
mattmc3 <mattmc3@gmail.com>
2019-09-14 22:16:01 -0400

    Add brew functionality

--- plugins/brew/brew.plugin.zsh
+++ plugins/brew/brew.plugin.zsh
@@ -1,5 +1,8 @@
+export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_CASK_OPTS="${HOMEBREW_CASK_OPTS:---appdir=/Applications}"
 alias bcu="brew cu"
+alias brewup="brew update && brew upgrade"
+alias caskup="brew cu -af"
 
 brew_export() {
   # brew is a pain... it dumps to a forced Brewfile, and is not consistently
