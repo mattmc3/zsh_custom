f342afb2d7a523cd7689c2d25dd9f22f133bffa0
mattmc3 <mattmc3@gmail.com>
2019-09-15 21:11:43 -0400

    Move lib functions into lib dir

--- lib/bzr.zsh
+++ lib/bzr.zsh
@@ -1,5 +1,5 @@
 # hollowed out - uses lazy loading
 
 # since this is the first file, add the functions dir for autoload
-fpath=("$ZSH_CUSTOM"/functions $fpath);
-autoload -U "$ZSH_CUSTOM"/functions/*(.:t)
+fpath=("$ZSH_CUSTOM"/lib/functions $fpath);
+autoload -U "$ZSH_CUSTOM"/lib/functions/*(.:t)
