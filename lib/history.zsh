c9289956f0fceaf8c2af267437032d372f225159
mattmc3 <mattmc3@gmail.com>
2019-11-29 22:52:03 -0500

    Fix history
    - Remove if statement that wasn't necessary
    - Add symlink to .zsh_history in $ZDOTDIR

--- lib/history.zsh
+++ lib/history.zsh
@@ -2,13 +2,14 @@
 HIST_STAMPS="${HIST_STAMPS:-yyyy-mm-dd}"
 source "$ZSH"/lib/history.zsh
 
-if [[ -z $HISTFILE ]]; then
-  if [[ -n "$XDG_DATA_HOME" ]]; then
-    HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zhistory"
-    [[ -f "$HISTFILE" ]] || { mkdir -p $(dirname "$HISTFILE") && touch $HISTFILE }
-  else
-    HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
+if [[ -n "$XDG_DATA_HOME" ]]; then
+  HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zhistory"
+  [[ -f "$HISTFILE" ]] || { mkdir -p $(dirname "$HISTFILE") && touch $HISTFILE }
+  if [[ "$ZDOTDIR" != "$HOME" ]] && [[ ! -L "$ZDOTDIR"/.zsh_history ]]; then
+    ln -sf "$HISTFILE" "$ZDOTDIR"/.zsh_history
   fi
+else
+  HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
 fi
 HISTSIZE=50000
 SAVEHIST=10000
