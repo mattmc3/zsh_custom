ddfff316873268d36de39d11359022f6ca14e93f
mattmc3 <mattmc3@gmail.com>
2019-12-09 22:34:34 -0500

    Change to .zshistory

--- lib/history.zsh
+++ lib/history.zsh
@@ -5,8 +5,8 @@ source "$ZSH"/lib/history.zsh
 if [[ -n "$XDG_DATA_HOME" ]]; then
   HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zhistory"
   [[ -f "$HISTFILE" ]] || { mkdir -p $(dirname "$HISTFILE") && touch $HISTFILE }
-  if [[ "$ZDOTDIR" != "$HOME" ]] && [[ ! -L "$ZDOTDIR"/.zsh_history ]]; then
-    ln -sf "$HISTFILE" "$ZDOTDIR"/.zsh_history
+  if [[ "$ZDOTDIR" != "$HOME" ]] && [[ ! -L "$ZDOTDIR"/.zhistory ]]; then
+    ln -sf "$HISTFILE" "$ZDOTDIR"/.zhistory
   fi
 else
   HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
