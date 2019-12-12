c71f63d657c7f4a76393a66dda76cf1cf06741cc
mattmc3 <mattmc3@gmail.com>
2019-12-12 18:34:51 -0500

    Update g to git function that defaults to git status

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,11 +1,18 @@
 # I don't care for all the aliases in the existing omz git plugin
 
 alias get="git"
-alias g="git"
 alias gadd="git add ."
 alias gcom="git checkout master"
 alias gcob="git checkout -b"
 alias sourcetree='open -a SourceTree'
+
+function g {
+  if [[ "$#" -eq 0 ]]; then
+    git status
+  else
+    git "$@"
+  fi
+}
 
 function gacp {
   local yn reply curbranch
