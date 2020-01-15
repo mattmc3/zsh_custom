34109560223673838990b88cdaf4a301167b20f3
mattmc3 <mattmc3@gmail.com>
2020-01-15 11:36:26 -0500

    Add spaceship prompt

--- plugins/prompt/prompt.plugin.zsh
+++ plugins/prompt/prompt.plugin.zsh
@@ -1,2 +1,8 @@
 # extend rather than override
 zstyle -t ":prezto:module:_prezto" loaded 'yes' && prezto-extend-module prompt
+
+if [[ -d $ZSH_CUSTOM ]] && [[ ! -d $ZSH_CUSTOM/plugins/prompt/external/spaceship ]]; then
+  git clone --depth 1 --recursive https://github.com/denysdovhan/spaceship-prompt.git $ZSH_CUSTOM/plugins/prompt/external/spaceship
+  mkdir -p "$ZSH_CUSTOM/plugins/prompt/functions"
+  ln -sf "$ZSH_CUSTOM/plugins/prompt/external/spaceship/spaceship.zsh" "$ZSH_CUSTOM/plugins/prompt/functions/prompt_spaceship_setup"
+fi
