24cd6d809f8477ddb5c1f73ff511176ab87d70cd
mattmc3 <mattmc3@gmail.com>
2020-09-05 18:52:26 -0400

    grep.zsh caching changes were merged upstream

--- lib/grep.zsh
+++
@@ -1,8 +0,0 @@
-_grep_alias_cache=("$ZSH_CACHE_DIR"/grep_alias.zsh(Nm-24))
-if (( $#_grep_alias_cache )); then
-  source "$ZSH_CACHE_DIR"/grep_alias.zsh
-else
-  source "$ZSH"/lib/grep.zsh
-  echo "alias $(alias grep)" >| "$ZSH_CACHE_DIR"/grep_alias.zsh
-fi
-unset _grep_alias_cache
