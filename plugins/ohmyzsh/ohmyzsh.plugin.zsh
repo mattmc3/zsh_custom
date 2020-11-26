580f697885f7dc3fcea845e8cdd79d9b02681b11
mattmc3 <mattmc3@gmail.com>
2020-11-25 21:33:02 -0500

    Updates to ohmyzsh plugin

--- plugins/ohmyzsh/ohmyzsh.plugin.zsh
+++ plugins/ohmyzsh/ohmyzsh.plugin.zsh
@@ -1,14 +1,4 @@
-if [[ ! -d "$ZSH" ]]; then
-  echo "ohmyzsh not installed or misconfigured: $ZSH" >&2
-  return 1
-fi
-
-# omz admin
-function omzup {
-  if [[ -z $ZSH ]] || [[ -z $ZSH_CUSTOM ]]; then
-    echo "OMZ not configured correctly or not enabled" >&2
-    return 1
-  fi
-  env ZSH=$ZSH ZSH_CACHE_DIR=$ZSH_CACHE_DIR DISABLE_UPDATE_PROMPT=true zsh -f $ZSH/tools/check_for_upgrade.sh
-  find $ZSH_CUSTOM -type d -name .git -prune -print -execdir git pull \;
-}
+for _f in "${0:h}/functions"/*(.N); do
+  autoload -Uz "$_f"
+done
+unset _f
