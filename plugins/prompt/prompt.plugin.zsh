cde5119bcafc8d48cf07ac9e18f624f6a6a33caf
mattmc3 <mattmc3@gmail.com>
2020-11-25 21:52:57 -0500

    Remove prompt plugin

--- plugins/prompt/prompt.plugin.zsh
+++
@@ -1,8 +0,0 @@
-0=${(%):-%N}
-extend-framework-plugin "${0:r:r}"
-
-if [[ -d $ZSH_CUSTOM ]] && [[ ! -d $ZSH_CUSTOM/plugins/prompt/external/spaceship ]]; then
-  git clone --depth 1 --recursive https://github.com/denysdovhan/spaceship-prompt.git $ZSH_CUSTOM/plugins/prompt/external/spaceship
-  mkdir -p "$ZSH_CUSTOM/plugins/prompt/functions"
-  ln -sf "$ZSH_CUSTOM/plugins/prompt/external/spaceship/spaceship.zsh" "$ZSH_CUSTOM/plugins/prompt/functions/prompt_spaceship_setup"
-fi
