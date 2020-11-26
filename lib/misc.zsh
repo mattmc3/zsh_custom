50e0588fb7209fe88477daee0fad0b88662bc596
mattmc3 <mattmc3@gmail.com>
2020-11-26 08:52:11 -0500

    Fix bug with variable assignment

--- lib/misc.zsh
+++ lib/misc.zsh
@@ -1,3 +1,3 @@
 # https://apple.stackexchange.com/questions/312795/zsh-paste-from-the-clipboard-a-command-takes-a-few-second-to-be-write-in-the-ter
-DISABLE_MAGIC_FUNCTIONS={$DISABLE_MAGIC_FUNCTIONS:-true}
+DISABLE_MAGIC_FUNCTIONS="${DISABLE_MAGIC_FUNCTIONS:-true}"
 source $ZSH/lib/misc.zsh
