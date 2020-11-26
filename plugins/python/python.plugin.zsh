78c19cb0ed75c400569185ad65b3d2f43fd5f61d
mattmc3 <mattmc3@gmail.com>
2020-11-25 21:39:49 -0500

    Updates and fixes for the Python plugin

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -1,6 +1,3 @@
-0=${(%):-%N}
-# extend-framework-plugin "${0:r:r}"
-
 if [[ -z $WORKON_HOME ]]; then
   export WORKON_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/venvs
   [[ -d "$WORKON_HOME" ]] || { mkdir -p "$WORKON_HOME" }
@@ -18,40 +15,7 @@ alias pyva="source .venv/bin/activate"
 # python: tab complete for workon dir (virtualenv)
 compdef '_files -W "$WORKON_HOME"' workon &> /dev/null
 
-pyclean() {
-  find . -type f -name "*.py[co]" -delete
-  find . -type d -name "__pycache__" -delete
-}
-
-pip_export() {
-  __pip_export "pip" "$@"
-}
-
-pip2_export() {
-  __pip_export "pip2" "$@"
-}
-
-pip3_export() {
-  __pip_export "pip3" "$@"
-}
-
-__pip_export() {
-  $1 freeze
-}
-
-pip_import() {
-  __pip_import "pip" "$@"
-}
-
-pip2_import() {
-  __pip_import "pip2" "$@"
-}
-
-pip3_import() {
-  __pip_import "pip3" "$@"
-}
-
-__pip_import() {
-  [[ -f "$2" ]] || { echo "Usage: Expecting valid requirements.txt path" >&2; return; }
-  $1 install -r "$2"
-}
+for _f in "${0:h}/functions"/*(.N); do
+  autoload -Uz "$_f"
+done
+unset _f
