7b0112cb39478264e5a831e75d6c118cfb4b8a14
mattmc3 <mattmc3@gmail.com>
2020-12-11 14:34:29 -0500

    Include function paths for custom prezto modules

--- plugins/prezto/prezto.plugin.zsh
+++ plugins/prezto/prezto.plugin.zsh
@@ -1,6 +1,14 @@
 function source_base_plugin() {
+  local zfunc
   if [[ -f "${ZPREZTODIR}/modules/${1}/init.zsh" ]]; then
+    if [[ -d "$ZPREZTODIR/modules/${1}/functions" ]]; then
+      fpath=("$ZPREZTODIR/modules/${1}/functions" $fpath)
+      for zfunc in "$ZPREZTODIR/modules/${1}/functions"/*(.N); do
+        autoload -Uz "$zfunc"
+      done
+    fi
     source "${ZPREZTODIR}/modules/${1}/init.zsh"
+
   elif [[ -f "${ZSH}/plugins/${1}/${1}.plugin.zsh" ]]; then
     source "${ZSH}/plugins/${1}/${1}.plugin.zsh"
   fi
