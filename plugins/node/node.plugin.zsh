c8c6ead617d423e1846bac3a7adbfffbab949991
mattmc3 <mattmc3@gmail.com>
2022-09-21 22:09:30 -0400

    Add node plugin

---
+++ plugins/node/node.plugin.zsh
@@ -0,0 +1,7 @@
+path=(
+  $path
+  /{usr,opt}/{homebrew,local}/share/npm/bin(N)
+)
+
+export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-$XDG_CONFIG_HOME/npm/npmrc}"
+export NODE_REPL_HISTORY="${NODE_REPL_HISTORY:-$XDG_DATA_HOME/nodejs/repl_history}"
