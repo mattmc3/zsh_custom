276ecd7e360a449831a0a0b9c48a76fd6c801d5f
mattmc3 <mattmc3@gmail.com>
2022-09-21 22:09:42 -0400

    Add ruby plugin

---
+++ plugins/ruby/ruby.plugin.zsh
@@ -0,0 +1,15 @@
+path=(
+  $path
+  /{usr,opt}/{homebrew,local}/opt/ruby/bin(N)
+  /{usr,opt}/{homebrew,local}/lib/ruby/gems/*/bin(N)
+  $HOME/.gem/ruby/*/bin(N)
+)
+
+# ruby bundler
+export BUNDLE_USER_CONFIG="${BUNDLE_USER_CONFIG:-$XDG_CONFIG_HOME/bundle}"
+export BUNDLE_USER_CACHE="${BUNDLE_USER_CACHE:-$XDG_CACHE_HOME/bundle}"
+export BUNDLE_USER_PLUGIN="${BUNDLE_USER_PLUGIN:-$XDG_DATA_HOME/bundle}"
+
+# ruby gems
+export GEM_HOME="${GEM_HOME:-$XDG_DATA_HOME/gem}"
+export GEM_SPEC_CACHE="${GEM_SPEC_CACHE:-$XDG_CACHE_HOME/gem}"
