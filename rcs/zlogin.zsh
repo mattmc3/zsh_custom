88556a216876dc23591c750cf2c1e81787ec2cf6
mattmc3 <mattmc3@gmail.com>
2022-09-21 22:10:38 -0400

    Add rcs

---
+++ rcs/zlogin.zsh
@@ -0,0 +1,9 @@
+# Execute code that does not affect the current session in the background.
+{
+  # Compile the completion dump to increase startup speed.
+  zcompdump="${XDG_CACHE_HOME:-$HOME/.cache}/zsh/zcompdump"
+  [[ -f "${zcompdump}" ]] || mkdir -p "${zcompdump:h}"
+  if [[ -s "$zcompdump" && (! -s "${zcompdump}.zwc" || "$zcompdump" -nt "${zcompdump}.zwc") ]]; then
+    zcompile "$zcompdump"
+  fi
+} &!
---
+++ rcs/zshrc.zsh
@@ -0,0 +1,57 @@
+# load zprof first if we need to profile
+[[ ${ZPROFRC:-0} -eq 0 ]] || zmodload zsh/zprof
+alias zprofrc="ZPROFRC=1 zsh"
+
+# setup zsh_custom
+ZSH_CUSTOM=$ZDOTDIR/custom
+[[ -d $ZSH_CUSTOM ]] || git clone git@github.com:mattmc3/zsh_custom $ZSH_CUSTOM
+fpath+=$ZSH_CUSTOM/functions
+autoload -Uz zcustominit && zcustominit
+
+plugins=(
+  # first
+  xdg-basedir
+  p10k-instant-prompt
+  prompt
+
+  abbreviations
+  aliases
+  autosuggestions
+  colors
+  completion
+  confd
+  directory
+  editor
+  emacs
+  environment
+  fancy-ctrl-z
+  git
+  golang
+  groovy
+  history
+  lpass
+  macos
+  node
+  python
+  ruby
+  string
+  syntax-highlighting
+  terminal
+  utility
+  z
+  zman
+
+  # last
+  history-substring-search
+)
+
+for plugin in $plugins; do
+  source "${ZSH_CUSTOM}/plugins/${plugin}/${plugin}.plugin.zsh"
+done
+unset plugin
+
+# local settings
+[[ ! -f $DOTFILES.local/zsh/zshrc_local.zsh ]] || source $DOTFILES.local/zsh/zshrc_local.zsh
+
+# done profiling
+[[ ${ZPROFRC:-0} -eq 0 ]] || { unset ZPROFRC && zprof }
