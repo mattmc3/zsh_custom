1077e0b136e58761a5343722bd6acd87b1d2ade7
mattmc3 <mattmc3@gmail.com>
2023-08-21 19:08:56 -0400

    Fix confd

--- plugins/confd/confd.plugin.zsh
+++ plugins/confd/confd.plugin.zsh
@@ -2,16 +2,9 @@
 # confd - Source all zsh files from a Fish-like conf.d directory
 #
 
-_confd=(
-  ${ZDOTDIR:-$HOME}/.zshrc.d(N/)
-  ${ZDOTDIR:-$HOME}/zshrc.d(N/)
-  ${ZDOTDIR:-$HOME/.config/zsh}/conf.d(N/)
-  ${ZDOTDIR:-$HOME/.config/zsh}/rc.d(N/)
-)
-(( $#_confd )) || return
-for _f in $_confd[1]/*.zsh(N); do
+for confd in $ZSH_CUSTOM/conf.d/*.zsh(.N); do
   # ignore files that begin with ~
-  case ${_f:t} in '~'*) continue;; esac
-  source "$_f"
+  case ${confd:t} in '~'*) continue;; esac
+  source $confd
 done
-unset _f _confd
+unset confd
