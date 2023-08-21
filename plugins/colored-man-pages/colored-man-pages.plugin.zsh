91bc6c491616903dd85d9170423d6104bb9684ae
mattmc3 <mattmc3@gmail.com>
2023-08-21 19:08:14 -0400

    Change color to colored-man-pages

---
+++ plugins/colored-man-pages/colored-man-pages.plugin.zsh
@@ -0,0 +1,24 @@
+#
+# color - Make terminal things more colorful.
+#
+
+#
+# Requirements
+#
+
+[[ "$TERM" != 'dumb' ]] || return 1
+
+#
+# Variables
+#
+
+# Colorize man pages.
+# start/end - md/me:bold; us/ue:underline; so/se:standout;
+# colors    - 0-black; 1-red; 2-green; 3-yellow; 4-blue; 5-magenta; 6-cyan; 7-white;
+export LESS_TERMCAP_mb=$'\e[01;36m'
+export LESS_TERMCAP_md=$'\e[01;36m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[00;47;30m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[04;35m'
