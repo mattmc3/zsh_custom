ef80b6f4f59cd7e2c9af465c46f116333fd38bf7
mattmc3 <mattmc3@gmail.com>
2023-08-21 18:26:17 -0400

    Update autosuggest

--- plugins/autosuggestions/autosuggestions.plugin.zsh
+++ plugins/autosuggestions/autosuggestions.plugin.zsh
@@ -2,7 +2,7 @@
 # autosuggestions - Fish-like Zsh autosuggestions
 #
 
-plugin-load zsh-users/zsh-autosuggestions
+plugin-source zsh-users/zsh-autosuggestions
 
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
 if [[ -n "$key_info" ]]; then
