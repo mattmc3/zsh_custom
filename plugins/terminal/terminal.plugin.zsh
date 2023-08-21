68febb1206b47e6eb35348a25c87a71758f71b95
mattmc3 <mattmc3@gmail.com>
2023-08-21 19:10:35 -0400

    Remove terminal plugin

--- plugins/terminal/terminal.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#
-# terminal - Set terminal window and tab titles.
-#
-# https://github.com/sorin-ionescu/prezto/tree/master/modules/terminal
-
-plugin-load sorin-ionescu/prezto/modules/terminal
-# plugin-load olets/zsh-window-title
