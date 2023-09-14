f505161ec8ec9d73a4f5e24749ff7528085973f3
mattmc3 <mattmc3@gmail.com>
2023-09-13 21:07:18 -0400

    Add clipboard plugin

---
+++ plugins/clipboard/clipboard.plugin.zsh
@@ -0,0 +1,4 @@
+source $ZSH/lib/clipboard.zsh
+source $ZSH/plugins/copyfile/copyfile.plugin.zsh
+source $ZSH/plugins/copypath/copypath.plugin.zsh
+source $ZSH/plugins/copybuffer/copybuffer.plugin.zsh
