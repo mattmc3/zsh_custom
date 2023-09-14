069116314af1269e7e12b2d71574f77db5444c4f
mattmc3 <mattmc3@gmail.com>
2023-09-14 18:33:30 -0400

    Do not use insta-prompt during profiling

--- plugins/p10k-instant-prompt/p10k-instant-prompt.plugin.zsh
+++ plugins/p10k-instant-prompt/p10k-instant-prompt.plugin.zsh
@@ -1,3 +1,9 @@
+#
+# Requirements
+#
+
+[[ -z "$ZPROFRC" ]] || return 1
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zsh/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
