e8f78e2edee3442e7db2511b758fbc11c1aed388
mattmc3 <mattmc3@gmail.com>
2023-09-20 15:45:02 -0400

    Update zfuncs

--- plugins/zfunctions/zfunctions.plugin.zsh
+++ plugins/zfunctions/zfunctions.plugin.zsh
@@ -11,8 +11,8 @@ autoload -Uz ${0:A:h}/functions/*(.:t)
 [[ -d "$ZFUNCDIR" ]] || ZFUNCDIR=$ZDOTDIR/functions
 
 # Load zfunctions subdirs.
-for _fndir in $ZFUNCDIR(N/) $ZFUNCDIR/*(N/); do
-  fpath=("$_fndir" $fpath)
-  autoload -Uz $fpath[1]/*(.:t)
+for _fndir in $ZFUNCDIR(FN/) $ZFUNCDIR/*(FN/); do
+  fpath=($_fndir $fpath)
+  autoload -Uz $_fndir/*(.:t)
 done
 unset _fndir
