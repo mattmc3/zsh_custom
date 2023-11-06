beb33a8d4bd2f6125569b51a1b14ac4d347cf4f6
mattmc3 <mattmc3@gmail.com>
2023-11-06 13:07:35 -0500

    Fix prompt plugin

--- plugins/prompt/prompt.plugin.zsh
+++ plugins/prompt/prompt.plugin.zsh
@@ -27,7 +27,7 @@ autoload -Uz promptinit && promptinit
 
 # Set the prompt if specified
 local -a prompt_theme
-zstyle -a ':zephyr:plugin:prompt' theme 'prompt_argv'
+zstyle -a ':zsh_custom:plugin:prompt' theme 'prompt_argv'
 if [[ $TERM == (dumb|linux|*bsd*) ]]; then
   prompt 'off'
 elif (( $#prompt_argv > 0 )); then
