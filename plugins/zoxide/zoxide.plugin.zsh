8a67636a6038a4d41a90bc26950d24225439c5c9
mattmc3 <mattmc3@gmail.com>
2023-12-20 12:21:01 -0500

    Update zoxide

--- plugins/zoxide/zoxide.plugin.zsh
+++ plugins/zoxide/zoxide.plugin.zsh
@@ -1,7 +1,15 @@
 : ${__zsh_cache_dir:=${XDG_CACHE_HOME:-$HOME/.cache}/zsh}
 
-typeset -a _cache_files=($__zsh_cache_dir/zcust_zoxide.zsh(Nmh-20))
-if ! (( $#_cache_files )); then
-  zoxide init zsh 2> /dev/null >| $__zsh_cache_dir/zcust_zoxide.zsh
+if ! (( $+commands[zoxide] )); then
+  echo >&2 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
+  return 1
 fi
-source $__zsh_cache_dir/zcust_zoxide.zsh
+
+typeset _initfile=$__zsh_cache_dir/zoxide.init.zsh
+typeset -a _cache=($initfile(Nmh-20))
+if ! (( $#_cache )); then
+  zoxide init zsh 2> /dev/null >| $_initfile
+fi
+source $_initfile
+
+unset _{cache,initfile}
