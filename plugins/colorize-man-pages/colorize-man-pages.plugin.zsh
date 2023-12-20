a87a72b1bd965bf3d2f27e5f104f4072e001a59e
mattmc3 <mattmc3@gmail.com>
2023-12-20 09:53:06 -0500

    Rename man page colorizer

--- plugins/colorize-man-pages/colorize-man-pages.plugin.zsh
+++ plugins/colorize-man-pages/colorize-man-pages.plugin.zsh
@@ -1,18 +1,7 @@
 #
-# color - Make terminal things more colorful.
+# colorize-man-pages: Make man more colorful.
 #
 
-#
-# Requirements
-#
-
-[[ "$TERM" != 'dumb' ]] || return 1
-
-#
-# Variables
-#
-
-# Colorize man pages.
 # start/end - md/me:bold; us/ue:underline; so/se:standout;
 # colors    - 0-black; 1-red; 2-green; 3-yellow; 4-blue; 5-magenta; 6-cyan; 7-white;
 export LESS_TERMCAP_mb=$'\e[01;36m'
