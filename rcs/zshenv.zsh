dcd3d131e6ed5f27941de92168c039465f824294
mattmc3 <mattmc3@gmail.com>
2024-03-04 21:49:45 -0500

    Update zshenv

--- rcs/zshenv.zsh
+++ rcs/zshenv.zsh
@@ -2,9 +2,9 @@
 # .zshenv - Define Zsh environment variables.
 #
 
-export ZDOTDIR=~/.config/zsh
+export ZDOTDIR=${ZDOTDIR:-$HOME/.config/zsh}
 
 # Use .zprofile for remaining environment.
-if [[ ( "$SHLVL" -eq 1 && ! -o LOGIN ) && -s "$ZDOTDIR/.zprofile" ]]; then
+if [[ ! -o LOGIN && -s "$ZDOTDIR/.zprofile" ]]; then
   source "$ZDOTDIR/.zprofile"
 fi
