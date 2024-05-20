e63147e53a1fecacc849937f96ed6d9c5f1273fd
mattmc3 <mattmc3@gmail.com>
2024-05-20 14:36:05 -0400

    directory: Update

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -1,5 +1,5 @@
 #
-# directory - Set directory options and define directory aliases.
+# directory: Set directory options and define directory aliases.
 #
 
 # References:
@@ -8,31 +8,38 @@
 # Return if requirements are not found.
 [[ "$TERM" != 'dumb' ]] || return 1
 
-# Set Zsh options related to directories, globbing, and I/O.
-setopt auto_pushd              # Make cd push the old directory onto the dirstack.
-setopt pushd_ignore_dups       # Don’t push multiple copies of the same directory onto the dirstack.
-setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent            # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home           # Push to home directory when no argument is given.
-setopt multios                 # Write to multiple descriptors.
-setopt extended_glob           # Use extended globbing syntax.
-setopt glob_dots               # Don't hide dotfiles from glob patterns.
-setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
-setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
+# 16.2.1 Changing Directories
+setopt auto_pushd         # Make cd push the old directory onto the dirstack.
+setopt pushd_minus        # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent       # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home      # Push to home directory when no argument is given.
+
+# 16.2.3 Expansion and Globbing
+setopt extended_glob      # Use more awesome globbing features.
+setopt glob_dots          # Include dotfiles when globbing.
+
+# 16.2.6 Input/Output
+setopt NO_clobber         # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent  # Ask for confirmation for `rm *' or `rm path/*'
+
+# 16.2.9 Scripts and Functions
+setopt multios        # Write to multiple descriptors.
 
 # Set aliases.
 if ! zstyle -t ':zsh_custom:plugin:directory:alias' skip; then
-  # directory aliases
+  # Set directory aliases.
   alias -- -='cd -'
   alias dirh='dirs -v'
 
   () {
-    local i
-    for i in {1..9}; do
-      # dirstack aliases (eg: "2"="cd 2")
-      alias "$i"="cd +${i}"
-      # backref aliases (eg: "..3"="../../..")
-      alias -g "..$i"=$(printf '../%.0s' {1..$i})
+    local index dotdots=".."
+    for index in {1..9}; do
+      alias "$index"="cd -${index}"   # dirstack aliases (eg: "3"="cd -3")
+      alias -g "..$index"="$dotdots"  # backref aliases (eg: "..3"="../../..")
+      dotdots+='/..'
     done
   }
 fi
+
+# Mark this plugin as loaded.
+zstyle ':zsh_custom:plugin:directory' loaded 'yes'
