77e65a64eb5cbb412daac83fb5a4741caadbaec2
mattmc3 <mattmc3@gmail.com>
2024-06-19 13:21:02 -0400

    Add prezto editor

--- plugins/editor/editor.plugin.zsh
+++ plugins/editor/editor.plugin.zsh
@@ -1,40 +1,19 @@
 #
 # editor - Set keybindings.
 #
-# https://github.com/sorin-ionescu/prezto/blob/master/modules/editor/init.zsh
 
-#
-# Requirements
-#
+# References:
+# - https://github.com/sorin-ionescu/prezto/blob/master/modules/editor/init.zsh
 
 # Return if requirements are not found.
 [[ "$TERM" != 'dumb' ]] || return 1
 
-#
-# Zstyles
-#
+# Set zero.
+0=${(%):-%N}
 
-# enable ../.. dot-expansion
-zstyle ':prezto:module:editor' dot-expansion 'yes'
-
-#
-# Init
-#
+# Enable ../.. dot-expansion unless explicitly disabled
+zstyle -T ':zsh_custom:module:editor' dot-expansion ||
+  zstyle ':zsh_custom:module:editor' dot-expansion 'yes'
 
 # Use Prezto's editor module.
-plugin-load sorin-ionescu/prezto/modules/editor
-
-#
-# Options
-#
-
-# Undo Prezto options.
-setopt NO_beep
-
-#
-# Keybindings
-#
-
-# https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/completion.zsh
-zmodload -i zsh/complist
-bindkey -M menuselect '^o' accept-and-infer-next-history
+source ${0:A:h}/external/prezto_editor.zsh
