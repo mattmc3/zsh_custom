301c99a33ac2c1bfe271801e4422dd941a47f988
mattmc3 <mattmc3@gmail.com>
2024-06-19 13:53:12 -0400

    Update python plugin

--- plugins/python/python.plugin.zsh
+++ plugins/python/python.plugin.zsh
@@ -9,14 +9,19 @@ export WORKON_HOME="$XDG_DATA_HOME/venvs"
 fpath=(${0:A:h}/functions $fpath)
 autoload -U $fpath[1]/*(.:t)
 
-alias py2='python2'
-alias py3='python3'
-alias py='python3'
-alias pip2update="pip2 list --outdated | cut -d ' ' -f1 | xargs -n1 pip2 install -U"
-alias pip3update="pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
+# Ensure python command exists.
+if (( $+commands[python3] )) && ! (( $+commands[python] )); then
+  alias python=python3
+fi
+if (( $+commands[pip3] )) && ! (( $+commands[pip] )); then
+  alias pip=pip3
+fi
+
+alias pipup="pip list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install -U"
 alias pyfind='find . -name "*.py"'
 alias pygrep='grep --include="*.py"'
 alias pyva="source .venv/bin/activate"
 
 # jupyter
-export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
+: ${JUPYTER_CONFIG_DIR:=${XDG_CONFIG_HOME:-$HOME/.config}/jupyter}
+export JUPYTER_CONFIG_DIR
