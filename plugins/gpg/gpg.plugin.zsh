6b19ad3bae6710943c370fbeb1366e70535bebf9
mattmc3 <mattmc3@gmail.com>
2024-06-19 13:53:03 -0400

    Update gpg plugin

--- plugins/gpg/gpg.plugin.zsh
+++ plugins/gpg/gpg.plugin.zsh
@@ -1,3 +1,7 @@
-export GNUPGHOME=${XDG_DATA_HOME:=~/.local/share}/gnupg
+#
+# gpg: Setup GnuPG
+#
+
+export GNUPGHOME=${XDG_DATA_HOME:-$HOME/.local/share}/gnupg
 [[ -e $GNUPGHOME:h ]] || mkdir -p $GNUPGHOME:h
 alias gpg="command gpg --homedir \"\$GNUPGHOME\""
