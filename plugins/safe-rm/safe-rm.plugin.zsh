327a69236e40fbd76f508ff33a8b73750859f409
mattmc3 <mattmc3@gmail.com>
2024-10-07 17:06:32 -0400

    Update safe-rm

--- plugins/safe-rm/safe-rm.plugin.zsh
+++ plugins/safe-rm/safe-rm.plugin.zsh
@@ -1,18 +1,3 @@
 export PATH="$ZSH_CUSTOM/.external/kaelzhang/shell-safe-rm/bin:$PATH"
-if [[ "$OSTYPE" == darwin* ]]; then
-  (( ${+commands[trash]} )) || brew install trash
-  function rm {
-    if [[ $# -eq 1 ]] && [[ -d "$1" ]]; then
-      rm.sh -r "$1"
-    else
-      rm.sh "$@"
-    fi
-  }
-  alias del="trash -F"
-  alias trash="trash -F"
-else
-  export PATH="$ZSH_CUSTOM/.external/kaelzhang/shell-safe-rm/bin:$PATH"
-  alias rm="rm.sh"
-  alias del="rm.sh"
-  alias trash="rm.sh"
-fi
+alias rm="rm.sh"
+alias del="rm.sh"
