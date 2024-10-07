d410b25111017695b91949ba6b968f08fc65d802
mattmc3 <mattmc3@gmail.com>
2024-10-07 17:25:40 -0400

    Remove gpg plugin

--- plugins/gpg/gpg.plugin.zsh
+++
@@ -1,7 +0,0 @@
-#
-# gpg: Setup GnuPG
-#
-
-export GNUPGHOME=${XDG_DATA_HOME:-$HOME/.local/share}/gnupg
-[[ -e $GNUPGHOME:h ]] || mkdir -p $GNUPGHOME:h
-alias gpg="command gpg --homedir \"\$GNUPGHOME\""
