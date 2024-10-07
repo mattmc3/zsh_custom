7d02469fe7c9b775e8aa5d510312175044cd0eae
mattmc3 <mattmc3@gmail.com>
2024-10-07 17:18:01 -0400

    Update zfunctions

--- plugins/zfunctions/zfunctions.plugin.zsh
+++ plugins/zfunctions/zfunctions.plugin.zsh
@@ -2,11 +2,26 @@
 # zfunctions - Use a Fish-like functions directory for zsh functions.
 #
 
+function autoload-dir {
+  local zdir
+  local -a zautoloads
+  for zdir in $@; do
+    [[ -n "$zdir" ]] || continue
+    if [[ ! -d $zdir ]]; then
+      echo >&2 "autoload-dir: Directory not found '$zdir'."
+      return 1
+    fi
+    fpath=("$zdir" $fpath)
+    zautoloads=($zdir/*~_*(N.:t))
+    (( $#zautoloads > 0 )) && autoload -Uz $zautoloads
+  done
+}
+
 # Load this plugin's functions.
 autoload-dir "${0:a:h}/functions"
 
 # Set ZFUNCDIR.
-: ${ZFUNCDIR:=$__zsh_config_dir/functions}
+: ${ZFUNCDIR:=${ZDOTDIR:-$HOME/.config/zsh}/functions}
 
 # Load zfunctions and any subdirs (F-full, N-nullglob, /-dirs).
 autoload-dir $ZFUNCDIR(FN/) $ZFUNCDIR/*(FN/)
