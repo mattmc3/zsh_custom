51afef54e9e942a5ced7d3846cf06b703bde89ad
mattmc3 <mattmc3@gmail.com>
2025-02-10 17:53:08 -0500

    Rename plugin

--- plugins/safe-rm/safe-rm.plugin.zsh
+++
@@ -1,3 +0,0 @@
-path+=("$ZSH_CUSTOM/.external/kaelzhang/shell-safe-rm/bin")
-alias rm="rm.sh"
-alias del="rm.sh"
---
+++ plugins/shell-safe-rm/shell-safe-rm.plugin.zsh
@@ -0,0 +1,3 @@
+path+=("$ZSH_CUSTOM/.external/shell-safe-rm/bin")
+alias rm="rm.sh"
+alias del="rm.sh"
