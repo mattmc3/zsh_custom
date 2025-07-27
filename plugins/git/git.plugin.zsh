b9d5e45c9945f1baabd9e4580812d02b41c6a683
mattmc3 <mattmc3@gmail.com>
2025-07-27 11:03:20 -0400

    Add git completions

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -5,17 +5,19 @@
 # References
 # - https://www.oliverspryn.com/blog/adding-git-completion-to-zsh
 
+0=${(%):-%N}
+
 function update_git_completions {
   # Download the git scripts
   local giturl=https://raw.githubusercontent.com/git/git/master/contrib/completion
-  local dest=${1:-${XDG_DATA_HOME:-~/.local/share}/zsh/completions}
+  local dest=${1:-${0:a:h}/completions}
   [[ -d $dest ]] || mkdir -p $dest
   curl -fsSL $giturl/git-completion.bash -o $dest/git-completion.bash
   curl -fsSL $giturl/git-completion.zsh -o $dest/_git
 }
 
 # Add completions
-fpath=(${XDG_DATA_HOME:-~/.local/share}/zsh/completions $fpath)
+fpath=(${0:a:h}/completions $fpath)
 [[ -f ${fpath[1]}/_git ]] || update_git_completions
 zstyle ':completion:*:*:git:*' script ${fpath[1]}/git-completion.bash
 
