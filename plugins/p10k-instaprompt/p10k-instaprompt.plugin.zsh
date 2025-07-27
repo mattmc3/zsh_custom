5b20a03905dd3885a976ef41e8bb000f3cde2897
mattmc3 <mattmc3@gmail.com>
2025-07-27 08:26:10 -0400

    Add p10k instant prompt plugin

---
+++ plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
@@ -0,0 +1,4 @@
+# Run this early so we can P10k instant prompt if we need it.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
