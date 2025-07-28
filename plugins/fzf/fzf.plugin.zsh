6da3233f102a6ab2ae5e13cffe3525598a55b7ec
mattmc3 <mattmc3@gmail.com>
2025-07-28 08:09:52 -0400

    Add fzf plugin

---
+++ plugins/fzf/fzf.plugin.zsh
@@ -0,0 +1,18 @@
+#
+# fzf: Configure the fuzzy finder.
+#
+
+if ! (( $+commands[fzf] )); then
+  echo "fzf not found" >&2
+  return 1
+fi
+
+# https://github.com/ajeetdsouza/fzf
+if (( $+functions[cached-eval] )); then
+  cached-eval 'fzf-zsh' fzf zsh
+else
+  source <(fzf --zsh)
+fi
+
+# Mark the plugin as loaded
+zstyle ':zsh_custom:plugin:fzf' loaded 'yes'
