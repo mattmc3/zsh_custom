1c2064f0be83a23b539050af84940e37472e64f6
mattmc3 <mattmc3@gmail.com>
2025-07-28 07:59:07 -0400

    Update hss

--- plugins/history-substring-search/history-substring-search.plugin.zsh
+++ plugins/history-substring-search/history-substring-search.plugin.zsh
@@ -1,21 +1,53 @@
 #!/bin/zsh
 # https://github.com/zsh-users/zsh-history-substring-search
 
-source "$ZSH_REPO_HOME/zsh-users/zsh-history-substring-search/zsh-history-substring-search.zsh"
-
-# Bind terminal-specific up and down keys
-if [[ -n "$terminfo[kcuu1]" ]]; then
-  bindkey -M emacs "$terminfo[kcuu1]" history-substring-search-up
-  bindkey -M viins "$terminfo[kcuu1]" history-substring-search-up
-fi
-if [[ -n "$terminfo[kcud1]" ]]; then
-  bindkey -M emacs "$terminfo[kcud1]" history-substring-search-down
-  bindkey -M viins "$terminfo[kcud1]" history-substring-search-down
+if (( ! $+functions[history-substring-search-up] )); then
+  source "$ZSH_REPO_HOME/zsh-users/zsh-history-substring-search/zsh-history-substring-search.zsh" || return 1
 fi
 
+zstyle -s ':zsh_custom:plugins:history-substring-search:color' found \
+  'HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND' \
+    || HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
+
+zstyle -s ':zsh_custom:plugins:history-substring-search:color' not-found \
+  'HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND' \
+    || HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+
+zstyle -s ':zsh_custom:plugins:history-substring-search' globbing-flags \
+  'HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS' \
+    || HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+
+if zstyle -t ':zsh_custom:plugins:history-substring-search' case-sensitive; then
+  HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS="${HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS//i}"
+fi
+
+if ! zstyle -t ':zsh_custom:plugins:history-substring-search' color; then
+  unset HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_{FOUND,NOT_FOUND}
+fi
+
+if zstyle -t ':zsh_custom:plugins:history-substring-search' fuzzy; then
+  HISTORY_SUBSTRING_SEARCH_FUZZY=1
+fi
+
+if zstyle -t ':zsh_custom:plugins:history-substring-search' unique; then
+  HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+fi
+
+if zstyle -t ':zsh_custom:plugins:history-substring-search' prefixed; then
+  HISTORY_SUBSTRING_SEARCH_PREFIXED=1
+fi
+
+# Key Bindings
 # Vi
 bindkey -M vicmd "k" history-substring-search-up
 bindkey -M vicmd "j" history-substring-search-down
+
+# Emacs and Vi
+for _keymap in 'emacs' 'viins'; do
+  bindkey -M "$keymap" "$terminfo[kcuu1]" history-substring-search-up
+  bindkey -M "$keymap" "$terminfo[kcud1]" history-substring-search-down
+done
+unset _keymap
 
 # Mark the plugin as loaded
 zstyle ':zsh_custom:plugin:history-substring-search' loaded 'yes'
