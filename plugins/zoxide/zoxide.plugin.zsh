e3c3d6c24139ac8edbe38800dfccd57d5ae63e2e
mattmc3 <mattmc3@gmail.com>
2025-07-28 08:09:47 -0400

    Update zoxide

--- plugins/zoxide/zoxide.plugin.zsh
+++ plugins/zoxide/zoxide.plugin.zsh
@@ -2,9 +2,16 @@
 # zoxide: Configure zoxide.
 #
 
+if ! (( $+commands[zoxide] )); then
+  echo "zoxide not found" >&2
+  return 1
+fi
+
 # https://github.com/ajeetdsouza/zoxide
-if (( $+commands[zoxide] )); then
+if (( $+functions[cached-eval] )); then
   cached-eval 'zoxide-init-zsh' zoxide init zsh
+else
+  source <(zoxide init zsh)
 fi
 
 # Mark the plugin as loaded
