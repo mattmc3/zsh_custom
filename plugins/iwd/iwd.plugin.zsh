fcc2010cbfa94994c44ff290bf6a3df4c78bd985
mattmc3 <mattmc3@gmail.com>
2025-07-29 08:28:16 -0400

    iwd: Check requirements

--- plugins/iwd/iwd.plugin.zsh
+++ plugins/iwd/iwd.plugin.zsh
@@ -2,8 +2,12 @@
 # iwd: initial working directory
 #
 
+# Check requirements.
+! zstyle -t ':zsh_custom:plugin:iwd' loaded || return 1
+
+# Set the initial working directory so we can easily return to it.
 : ${IWD:=$PWD}
 alias iwd='cd $IWD'
 
-# Mark the plugin as loaded
+# Mark the plugin as loaded.
 zstyle ':zsh_custom:plugin:iwd' loaded 'yes'
