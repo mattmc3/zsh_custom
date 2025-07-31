0e6212878b6d65beb518e5ec685e1669f3c0f195
mattmc3 <mattmc3@gmail.com>
2025-07-31 10:57:02 -0400

    Remove unneeded plugins

--- plugins/clipboard/clipboard.plugin.zsh
+++
@@ -1,9 +0,0 @@
-[[ -d "${ZSH:-?}" ]] || return 1
-
-source $ZSH/lib/clipboard.zsh
-source $ZSH/plugins/copybuffer/copybuffer.plugin.zsh
-source $ZSH/plugins/copyfile/copyfile.plugin.zsh
-source $ZSH/plugins/copypath/copypath.plugin.zsh
-
-# Mark the plugin as loaded
-zstyle ':zsh_custom:plugin:clipboard' loaded 'yes'
