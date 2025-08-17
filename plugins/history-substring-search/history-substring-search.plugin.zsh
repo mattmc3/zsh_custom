4e822cde0f6c130fe8d4b09854c71eda0991ddd7
mattmc3 <mattmc3@gmail.com>
2025-08-17 12:42:46 -0400

    Update keymap

--- plugins/history-substring-search/history-substring-search.plugin.zsh
+++ plugins/history-substring-search/history-substring-search.plugin.zsh
@@ -44,8 +44,8 @@ bindkey -M vicmd "j" history-substring-search-down
 
 # Emacs and Vi
 for _keymap in 'emacs' 'viins'; do
-  bindkey -M "$keymap" "$terminfo[kcuu1]" history-substring-search-up
-  bindkey -M "$keymap" "$terminfo[kcud1]" history-substring-search-down
+  bindkey -M "$_keymap" "$terminfo[kcuu1]" history-substring-search-up
+  bindkey -M "$_keymap" "$terminfo[kcud1]" history-substring-search-down
 done
 unset _keymap
 
