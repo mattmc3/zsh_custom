6fcf9de3d62292879008f2952b8505832696075d
mattmc3 <mattmc3@gmail.com>
2025-10-20 16:50:26 -0400

    Update postgres

--- plugins/postgres/postgres.plugin.zsh
+++ plugins/postgres/postgres.plugin.zsh
@@ -1,5 +1,11 @@
-# Add postgres to our path
-path+=(${HOMEBREW_PREFIX:-/opt/homebrew}/opt/postgresql@17/bin(/N))
+# Add postgres to path
+path+=(${HOMEBREW_PREFIX:-/opt/homebrew}/opt/postgresql@18/bin(/N))
+
+# postgres
+export PSQLRC="${PSQLRC:-$XDG_CONFIG_HOME/pg/psqlrc}"
+export PSQL_HISTORY="${PSQL_HISTORY:-$XDG_CACHE_HOME/pg/psql_history}"
+export PGPASSFILE="${PGPASSFILE:-$XDG_CONFIG_HOME/pg/pgpass}"
+export PGSERVICEFILE="${PGSERVICEFILE:-$XDG_CONFIG_HOME/pg/pg_service.conf}"
 
 # Mark the plugin as loaded
 zstyle ':zsh_custom:plugin:postgres' loaded 'yes'
