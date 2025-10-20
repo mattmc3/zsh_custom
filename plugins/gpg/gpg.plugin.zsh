66cb4cd77020e49bd556888b49d3cce965d1b760
mattmc3 <mattmc3@gmail.com>
2025-10-20 16:50:01 -0400

    Update gpg

--- plugins/gpg/gpg.plugin.zsh
+++ plugins/gpg/gpg.plugin.zsh
@@ -2,9 +2,10 @@
 # gpg: Customize GNUPG
 #
 
+# gpg
 export GNUPGHOME=${GNUPGHOME:=${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}
 [[ -e $GNUPGHOME:h ]] || mkdir -p $GNUPGHOME:h
-alias gpg="${aliases[gpg]:-gpg} --homedir \"$GNUPGHOME\""
+alias gpg="${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\""
 
 # Mark the plugin as loaded
 zstyle ':zsh_custom:plugin:gpg' loaded 'yes'
