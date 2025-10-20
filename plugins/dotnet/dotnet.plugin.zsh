79b7689257f85dffaf92d9ad6402ac82e13d061d
mattmc3 <mattmc3@gmail.com>
2025-10-20 16:49:42 -0400

    Update dotnet

--- plugins/dotnet/dotnet.plugin.zsh
+++ plugins/dotnet/dotnet.plugin.zsh
@@ -1,6 +1,9 @@
 # zsh parameter completion for the dotnet CLI
 # https://learn.microsoft.com/en-us/dotnet/core/tools/enable-tab-autocomplete
 
+# nuget
+export NUGET_PACKAGES="${NUGET_PACKAGES:-$XDG_CACHE_HOME/NuGetPackages}"
+
 _dotnet_zsh_complete()
 {
   local completions=("$(dotnet complete "$words")")
