d5aea7e55d4cda7e38d5a3a3e4e40599e2fa0397
mattmc3 <mattmc3@gmail.com>
2026-02-02 17:38:39 -0500

    Remove dupe code from clipboard

--- plugins/clipboard/clipboard.plugin.zsh
+++ plugins/clipboard/clipboard.plugin.zsh
@@ -5,34 +5,6 @@
 # Return if requirements are not met.
 [[ "$TERM" != 'dumb' ]] || return 1
 
-# Use built-in paste magic.
-autoload -Uz bracketed-paste-url-magic
-zle -N bracketed-paste bracketed-paste-url-magic
-autoload -Uz url-quote-magic
-zle -N self-insert url-quote-magic
-
-# Ensure pbcopy/pbpaste commands exist.
-if ! (( $+commands[pbcopy] )); then
-  if [[ "$OSTYPE" == cygwin* ]]; then
-    alias pbcopy='tee > /dev/clipboard'
-    alias pbpaste='cat /dev/clipboard'
-  elif [[ "$OSTYPE" == linux-android ]]; then
-    alias pbcopy='termux-clipboard-set'
-    alias pbpaste='termux-clipboard-get'
-  elif (( $+commands[wl-copy] && $+commands[wl-paste] )); then
-    alias pbcopy='wl-copy'
-    alias pbpaste='wl-paste'
-  elif [[ -n $DISPLAY ]]; then
-    if (( $+commands[xclip] )); then
-      alias pbcopy='xclip -selection clipboard -in'
-      alias pbpaste='xclip -selection clipboard -out'
-    elif (( $+commands[xsel] )); then
-      alias pbcopy='xsel --clipboard --input'
-      alias pbpaste='xsel --clipboard --output'
-    fi
-  fi
-fi
-
 # copy a file
 copyfile() {
   emulate -L zsh
