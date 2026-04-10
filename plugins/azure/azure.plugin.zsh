007882ef2742f7ca28442df7eac0e57d39ab7d6e
mattmc3 <mattmc3@gmail.com>
2026-04-10 11:50:33 -0400

    Update azure

--- plugins/azure/azure.plugin.zsh
+++ plugins/azure/azure.plugin.zsh
@@ -1,3 +1,7 @@
+#
+# Azure
+#
+
 function azdbtok {
   local tok="$(az account get-access-token --resource https://ossrdbms-aad.database.windows.net --query accessToken --output tsv)"
   echo "$tok" | tee >(pbcopy) >(cat)
