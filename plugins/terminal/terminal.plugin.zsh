cb31b7826bd4decef603b2f26c7340713d06a32b
mattmc3 <mattmc3@gmail.com>
2026-04-10 11:45:19 -0400

    Update terminal

--- plugins/terminal/terminal.plugin.zsh
+++ plugins/terminal/terminal.plugin.zsh
@@ -1,3 +1,10 @@
+#
+# Terminal
+#
+
+0=${(%):-%N}
+MY_ZSH_CUSTOM=${0:a:h:h}
+
 # Emit an OSC 1337 sequence to set vars your terminal app (WezTerm) can use.
 function set_terminal_var() {
   hash base64 2>/dev/null || return 1
@@ -15,21 +22,32 @@ function set_terminal_var() {
 set_terminal_var "TERM_CURRENT_SHELL" "zsh ${ZSH_PATCHLEVEL:-$ZSH_VERSION}"
 
 
-function wezterm_update_shell_integration() {
+function update_wezterm_shell_integration() {
   curl -fsSL "https://raw.githubusercontent.com/wez/wezterm/main/assets/shell-integration/wezterm.sh" \
     -o "$MY_ZSH_CUSTOM/pkg/wezterm-shell-integration.sh"
 }
 
-if [[ "${TERM_PROGRAM:l}" == wezterm ]]; then
-  # Use WezTerm shell integration
-  source "$MY_ZSH_CUSTOM/pkg/wezterm-shell-integration.sh"
-
-  function set_current_shell_precmd() {
-    __wezterm_set_user_var "TERM_CURRENT_SHELL" "zsh ${${ZSH_PATCHLEVEL:-$ZSH_VERSION}#zsh-}"
-  }
-  autoload -Uz add-zsh-hook
-  add-zsh-hook precmd set_current_shell_precmd
-elif [[ "${TERM_PROGRAM:l}" == apple_terminal ]]; then
-  # Make Apple Terminal behave.
-  export SHELL_SESSIONS_DISABLE=${SHELL_SESSIONS_DISABLE:-1}
-fi
+case "${TERM_PROGRAM:l}" in
+  apple_terminal)
+    export SHELL_SESSIONS_DISABLE=${SHELL_SESSIONS_DISABLE:-1}
+    ;;
+  ghostty)
+    source ${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration
+    ;;
+  vscode)
+    # https://code.visualstudio.com/docs/terminal/shell-integration
+    MY_HISTFILE=${HISTFILE:-${XDG_DATA_HOME:-$HOME/.local/share}/zsh/zsh_history}
+    source "$(code --locate-shell-integration-path zsh)"
+    HISTFILE=$MY_HISTFILE
+    #unset MY_HISTFILE
+    ;;
+  wezterm)
+    source "$MY_ZSH_CUSTOM/pkg/wezterm-shell-integration.sh"
+    function set_current_shell() {
+      __wezterm_set_user_var "TERM_CURRENT_SHELL" "zsh ${${ZSH_PATCHLEVEL:-$ZSH_VERSION}#zsh-}"
+    }
+    set_current_shell
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd set_current_shell
+    ;;
+esac
