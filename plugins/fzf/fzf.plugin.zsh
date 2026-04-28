013faeca1eb6c3c2a88785f6e1d059f0fbe6fc05
mattmc3 <mattmc3@gmail.com>
2026-04-28 10:59:13 -0400

    Update fzf

--- plugins/fzf/fzf.plugin.zsh
+++ plugins/fzf/fzf.plugin.zsh
@@ -2,14 +2,22 @@
 # fzf: Configure the fuzzy finder.
 #
 
+# https://github.com/ajeetdsouza/fzf
 if ! (( $+commands[fzf] )); then
-  echo "fzf not found" >&2
+  print -ru2 "fzf not found"
   return 1
 fi
 
-# https://github.com/ajeetdsouza/fzf
-if (( $+functions[cached-eval] )); then
-  cached-eval 'fzf-zsh' fzf --zsh
+function init_fzf() {
+  if (( $+functions[cached-eval] )); then
+    cached-eval 'fzf-zsh' fzf --zsh
+  else
+    source <(fzf --zsh)
+  fi
+}
+
+if (( $+functions[zvm_version] )); then
+  zvm_after_init_commands+=(init_fzf)
 else
-  source <(fzf --zsh)
+  init_fzf
 fi
