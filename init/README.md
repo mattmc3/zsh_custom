4f03b6488c3f4c6bcd6aba6490f55c4708483af3
mattmc3 <mattmc3@gmail.com>
2026-05-01 19:39:35 -0400

    Remove unused scripts

--- init/README.md
+++
@@ -1,4 +0,0 @@
-# init
-
-Initialize Zsh to overcome Zsh's poor out-of-the-box experience. We are very careful
-not to put too much more here than is necessary.
--- plugins/history-substring-search/history-substring-search.plugin.zsh
+++ plugins/history-substring-search/history-substring-search.plugin.zsh
@@ -1,4 +1,23 @@
-#!/bin/zsh
+#
+# Requirements
+#
+
+0=${(%):-%N}
+MY_ZSH_CUSTOM=${0:a:h}
+
+zstyle -t ":zsh_custom:plugin:__init__" loaded ||
+  source $MY_ZSH_CUSTOM/plugins/__init__/__init__.plugin.zsh
+
+#
+# Init
+#
+
+antibody bundle zsh-users/zsh-history-substring-search
+
+#
+# Keybinds
+#
+
 zmodload zsh/terminfo
 
 # Emacs
@@ -15,3 +34,9 @@ for keymap in 'main' 'emacs' 'viins'; do
   bindkey -M "$keymap" "$terminfo[kcuu1]" history-substring-search-up
   bindkey -M "$keymap" "$terminfo[kcud1]" history-substring-search-down
 done
+
+#
+# Wrap up
+#
+
+zstyle ":zsh_custom:plugin:history-substring-search" loaded yes
