49b38fbd6e9fd606982e1390ef4d45c1032f46b8
mattmc3 <mattmc3@gmail.com>
2026-05-03 10:45:10 -0400

    Update zsh_custom

--- zsh_custom.zsh
+++ zsh_custom.zsh
@@ -3,19 +3,23 @@
 0=${(%):-%N}
 MY_ZSH_CUSTOM=${0:a:h}
 
-for _init in $MY_ZSH_CUSTOM/init/*.zsh; do
-  source $_init && _loaded=yes || _loaded=no
-  zstyle ":zsh_custom:init:${_init:t:r}" loaded $_loaded
+# Load libs
+for _zlib in "$MY_ZSH_CUSTOM"/lib/*.zsh; do
+  source $_zlib && _loaded=yes || _loaded=no
+  zstyle ":zsh_custom:lib:${_zlib:t}" loaded $_loaded
 done
 
-for _plugin in $plugins; do
-  fpath+=("$MY_ZSH_CUSTOM/plugins/${_plugin:t}"(N))
-  _plugin_init=$MY_ZSH_CUSTOM/plugins/${_plugin:t}/${_plugin:t}.plugin.zsh
-  if [[ ! -e "$_plugin_init" ]]; then
-    echo "Plugin not found: $_plugin_init" >&2
+# Load plugins
+for _zplugin in $plugins; do
+  fpath+=("$MY_ZSH_CUSTOM/plugins/${_zplugin:t}"(N))
+  _zplugin_init=$MY_ZSH_CUSTOM/plugins/${_zplugin:t}/${_zplugin:t}.plugin.zsh
+  if [[ ! -e "$_zplugin_init" ]]; then
+    echo "Plugin not found: $_zplugin_init" >&2
     continue
   else
-    source $_plugin_init && _loaded=yes || _loaded=no
-    zstyle ":zsh_custom:plugin:${_plugin:t}" loaded $_loaded
+    source $_zplugin_init && _loaded=yes || _loaded=no
+    zstyle ":zsh_custom:plugin:${_zplugin:t}" loaded $_loaded
   fi
 done
+
+unset _z{lib,plugin} _loaded
