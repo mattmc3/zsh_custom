51ca35dd2fde10afedb760368cd6c47c8ee5a2fe
mattmc3 <mattmc3@gmail.com>
2026-05-04 09:25:42 -0400

    Add completions

--- plugins/completions/completions.plugin.zsh
+++ plugins/completions/completions.plugin.zsh
@@ -1,8 +1,5 @@
+#
 antibody bundle zsh-users/zsh-completions path:src kind:fpath
 
-autoload -Uz compinit
-if (( $+functions[compinit-fast] )); then
-  compinit-fast
-else
-  compinit -i -d "$ZSH_COMPDUMP"
-fi
+# Initialize completions at the end
+hooks-add-hook postzshrc_hook compinit-fast
