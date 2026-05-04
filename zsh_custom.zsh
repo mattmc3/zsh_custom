f542983265a3915f4e16304d7a456d239a11dd58
mattmc3 <mattmc3@gmail.com>
2026-05-03 22:16:37 -0400

    Update zsh_custom loader

--- zsh_custom.zsh
+++ zsh_custom.zsh
@@ -3,23 +3,33 @@
 0=${(%):-%N}
 MY_ZSH_CUSTOM=${0:a:h}
 
-# Load libs
-for _zlib in "$MY_ZSH_CUSTOM"/lib/*.zsh; do
-  source $_zlib && _loaded=yes || _loaded=no
-  zstyle ":zsh_custom:lib:${_zlib:t}" loaded $_loaded
-done
+zmodload zsh/datetime
+
+() {
+  # Load libs
+  local zlib loaded
+  for zlib in "$MY_ZSH_CUSTOM"/lib/*.zsh; do
+    source $zlib && loaded=yes || loaded=no
+    zstyle ":zsh_custom:lib:${zlib:t}" loaded $loaded
+  done
+  unset zlib
+}
 
 # Load plugins
-for _zplugin in $plugins; do
-  fpath+=("$MY_ZSH_CUSTOM/plugins/${_zplugin:t}"(N))
-  _zplugin_init=$MY_ZSH_CUSTOM/plugins/${_zplugin:t}/${_zplugin:t}.plugin.zsh
-  if [[ ! -e "$_zplugin_init" ]]; then
-    echo "Plugin not found: $_zplugin_init" >&2
-    continue
-  else
-    source $_zplugin_init && _loaded=yes || _loaded=no
-    zstyle ":zsh_custom:plugin:${_zplugin:t}" loaded $_loaded
-  fi
-done
-
-unset _z{lib,plugin} _loaded
+() {
+  local zplugin zplugin_init loaded start_ts end_ts
+  for zplugin in $plugins; do
+    start_ts=$EPOCHREALTIME
+    fpath+=("$MY_ZSH_CUSTOM/plugins/${zplugin:t}"(N))
+    zplugin_init=$MY_ZSH_CUSTOM/plugins/${zplugin:t}/${zplugin:t}.plugin.zsh
+    if [[ ! -e "$zplugin_init" ]]; then
+      echo >&2 "Plugin not found: $zplugin_init"
+      continue
+    else
+      source $zplugin_init && loaded=yes || loaded=no
+      end_ts=$EPOCHREALTIME
+      zstyle ":zsh_custom:plugin:${zplugin:t}" loaded $loaded
+      zstyle ":zsh_custom:plugin:${zplugin:t}" load_time $(( end_ts - start_ts ))
+    fi
+  done
+}
