740550c8ed81e038c694f56d51228e6fbdffd9e2
mattmc3 <mattmc3@gmail.com>
2026-05-04 09:24:50 -0400

    Remove anonymous functions

--- zsh_custom.zsh
+++ zsh_custom.zsh
@@ -5,39 +5,34 @@ MY_ZSH_CUSTOM=${0:a:h}
 
 zmodload zsh/datetime
 
-() {
-  # Load libs
-  local zlib loaded
-  for zlib in "$MY_ZSH_CUSTOM"/lib/*.zsh; do
-    source $zlib && loaded=yes || loaded=no
-    zstyle ":zsh_custom:lib:${zlib:t}" loaded $loaded
-  done
-  unset zlib
-}
+# Load libs
+for _zlib in "$MY_ZSH_CUSTOM"/lib/*.zsh; do
+  source $_zlib && _zloaded=yes || _zloaded=no
+  zstyle ":zsh_custom:lib:${_zlib:t}" loaded $_zloaded
+done
 
 # Load plugins
-() {
-  local zplugin zplugin_name zplugin_init loaded start_ts
-  for zplugin in $plugins; do
-    start_ts=$EPOCHREALTIME
-    zplugin_name=${zplugin:t}
-    loaded=no
+#local _zplugin _zplugin_name _zplugin_init loaded _zstart_ts
+for _zplugin in $plugins; do
+  _zstart_ts=$EPOCHREALTIME
+  _zplugin_name=${_zplugin:t}
+  _zloaded=no
 
-    if [[ -d "$MY_ZSH_CUSTOM/plugins/${zplugin_name}" ]]; then
-      zplugin_init=$MY_ZSH_CUSTOM/plugins/${zplugin_name}/${zplugin_name}.plugin.zsh
-      if [[ -e "$zplugin_init" ]]; then
-        fpath+=("$MY_ZSH_CUSTOM/plugins/$zplugin_name")
-        source $zplugin_init && loaded=yes
-      else
-        echo >&2 "Plugin not found: $zplugin_init"
-      fi
-    elif [[ "$zplugin" == */* ]]; then
-      antibody bundle $zplugin && loaded=yes
+  if [[ -d "$MY_ZSH_CUSTOM/plugins/${_zplugin_name}" ]]; then
+    _zplugin_init=$MY_ZSH_CUSTOM/plugins/${_zplugin_name}/${_zplugin_name}.plugin.zsh
+    if [[ -e "$_zplugin_init" ]]; then
+      fpath+=("$MY_ZSH_CUSTOM/plugins/$_zplugin_name")
+      source $_zplugin_init && _zloaded=yes
     else
-      echo >&2 "Unknown plugin: $zplugin"
+      echo >&2 "Plugin not found: $_zplugin_init"
     fi
+  elif [[ "$_zplugin" == */* ]]; then
+    antibody bundle $_zplugin && _zloaded=yes
+  else
+    echo >&2 "Unknown plugin: $_zplugin"
+  fi
 
-    zstyle ":zsh_custom:plugin:$zplugin_name" loaded $loaded
-    zstyle ":zsh_custom:plugin:$zplugin_name" load_time $(( EPOCHREALTIME - start_ts ))
-  done
-}
+  zstyle ":zsh_custom:plugin:$_zplugin_name" loaded $_zloaded
+  zstyle ":zsh_custom:plugin:$_zplugin_name" load_time $(( EPOCHREALTIME - _zstart_ts ))
+done
+unset _zplugin{,_name,_init} _z{lib,loaded,start_ts}
