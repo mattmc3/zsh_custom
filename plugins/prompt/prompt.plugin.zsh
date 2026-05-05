ec8c77bfe90338189eccb3de180732a944ebd474
mattmc3 <mattmc3@gmail.com>
2026-05-04 20:11:08 -0400

    Update prompt

--- plugins/prompt/prompt.plugin.zsh
+++ plugins/prompt/prompt.plugin.zsh
@@ -1,12 +1,30 @@
-# Load plugin functions.
+#
+# prompt: Setup Zsh prompt
+#
 0=${(%):-%N}
+[[ "$TERM" == dumb ]] && return
+
+# Load our functions
 fpath=(${0:a:h}/functions $fpath)
 autoload -Uz ${0:a:h}/functions/*(.:t)
 
-antibody bundle romkatv/powerlevel10k kind:clone
-antibody bundle sindresorhus/pure kind:fpath
+# Powerlevel10k doesn't play nice with the built-in Zsh prompt system. You can't
+# use instaprompt and change prompts, so if using p10k, we might as well not even
+# run promptinit.
 
-autoload -Uz promptinit && promptinit
-if [[ -n "$ZSH_THEME" ]]; then
-  prompt ${(z)ZSH_THEME}
-fi
+_zsh_theme=(${(z)ZSH_THEME})
+case ${_zsh_theme[1]} in
+  p10k|powerlevel10k)
+    prompt_p10k_setup "${_zsh_theme[@][2,-1]}"
+    ;;
+  *)
+    antibody bundle romkatv/powerlevel10k kind:clone
+    antibody bundle sindresorhus/pure kind:fpath
+
+    autoload -Uz promptinit && promptinit
+    if (( $#_zsh_theme )); then
+      prompt ${_zsh_theme[@]}
+    fi
+    ;;
+esac
+unset _zsh_theme
