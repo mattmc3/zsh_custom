7069d01f3d4d02b68b8ced2d765a66bf444238de
mattmc3 <mattmc3@gmail.com>
2026-05-06 12:38:54 -0400

    Update zprofile

--- rcs/zprofile.zsh
+++ rcs/zprofile.zsh
@@ -1,33 +1,4 @@
 #!/bin/zsh
-#
-# Browser
-#
-
-if [[ -z "$BROWSER" && "$OSTYPE" == darwin* ]]; then
-  export BROWSER='open'
-fi
-
-#
-# Editors
-#
-
-if [[ -z "$EDITOR" ]]; then
-  export EDITOR='nvim'
-fi
-if [[ -z "$VISUAL" ]]; then
-  export VISUAL='code'
-fi
-if [[ -z "$PAGER" ]]; then
-  export PAGER='less'
-fi
-
-#
-# Language
-#
-
-if [[ -z "$LANG" ]]; then
-  export LANG='en_US.UTF-8'
-fi
 
 #
 # Paths
@@ -37,9 +8,9 @@ fi
 typeset -gU cdpath fpath mailpath path
 
 # Set the list of directories that cd searches.
-# cdpath=(
-#   $cdpath
-# )
+cdpath=(
+  $HOME/Projects/mattmc3(/N)
+)
 
 # Set the list of directories that Zsh searches for programs.
 path=(
@@ -50,20 +21,32 @@ path=(
 )
 
 #
+# Vars
+#
+
+export BROWSER='open'
+export EDITOR='nvim'
+export VISUAL='code'
+export PAGER='less'
+export LANG='en_US.UTF-8'
+
+#
 # Less
 #
 
 # Set the default Less options.
 # Mouse-wheel scrolling has been disabled by -X (disable screen clearing).
 # Remove -X to enable it.
-if [[ -z "$LESS" ]]; then
-  export LESS='-g -i -M -R -S -w -z-4'
-fi
+export LESS='-g -i -M -R -S -w -z-4'
 
 # Set the Less input preprocessor.
 # Try both `lesspipe` and `lesspipe.sh` as either might exist on a system.
 if [[ -z "$LESSOPEN" ]] && (( $#commands[(i)lesspipe(|.sh)] )); then
   export LESSOPEN="| /usr/bin/env $commands[(i)lesspipe(|.sh)] %s 2>&-"
 fi
+
+#
+# Misc
+#
 
 export SHELL_SESSIONS_DISABLE=1
