c9303178e453cf47bc7dca7e6271202e624d0e85
mattmc3 <mattmc3@gmail.com>
2026-05-23 14:00:39 -0400

    Update to use rbenv

--- plugins/ruby/ruby.plugin.zsh
+++ plugins/ruby/ruby.plugin.zsh
@@ -12,3 +12,24 @@ path+=(
   $HOME/.gem/ruby/*/bin(N)
   $path
 )
+
+# eval "$(rbenv init - --no-rehash zsh)"
+if (( ${+commands[rbenv]} )); then
+  [[ -d "$HOME/.rbenv" ]] || rbenv init
+  export PATH="$HOME/.rbenv/shims:${PATH}"
+  export RBENV_SHELL=zsh
+  rbenv() {
+    local command
+    command="${1:-}"
+    if [ "$#" -gt 0 ]; then
+      shift
+    fi
+
+    case "$command" in
+    rehash|shell)
+      eval "$(rbenv "sh-$command" "$@")";;
+    *)
+      command rbenv "$command" "$@";;
+    esac
+  }
+fi
