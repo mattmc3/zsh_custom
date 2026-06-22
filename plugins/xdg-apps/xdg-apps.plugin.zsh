2c0cf20c6731e2b2c1e187db90363ec29bfe2ff2
mattmc3 <mattmc3@gmail.com>
2026-06-22 16:43:57 -0400

    Fix xdg apps

--- plugins/xdg-apps/xdg-apps.plugin.zsh
+++ plugins/xdg-apps/xdg-apps.plugin.zsh
@@ -6,8 +6,11 @@ export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
-export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-$HOME/.xdg}
+if [[ -z "$XDG_RUNTIME_DIR" ]]; then
+  export XDG_RUNTIME_DIR="${${TMPDIR%/}:-/tmp}/xdg-runtime-$(id -u)"
+fi
 mkdir -p $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_STATE_HOME $XDG_RUNTIME_DIR
+chmod 700 "$XDG_RUNTIME_DIR" 2>/dev/null
 
 # readline
 export INPUTRC="${INPUTRC:-$XDG_CONFIG_HOME/readline/inputrc}"
