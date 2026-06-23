4c5743c83150ee7ecdf4471dfb134cd8e5ca3e6a
mattmc3 <mattmc3@gmail.com>
2026-06-23 12:31:05 -0400

    Fix XDG_RUNTIME_DIR

--- plugins/xdg-apps/apps/screen.zsh
+++ plugins/xdg-apps/apps/screen.zsh
@@ -1,2 +1,2 @@
 export SCREENRC="${SCREENRC:-${XDG_CONFIG_HOME:-$HOME/.config}/screen/screenrc}"
-export SCREENDIR="${SCREENDIR:-${XDG_RUNTIME_DIR:-$HOME/.xdg}/screen}"
+export SCREENDIR="${SCREENDIR:-${XDG_RUNTIME_DIR:-/tmp/xdg-runtime-$UID}/screen}"
