8f140e82cd3c8a41d413ee39d11768a0689180b0
mattmc3 <mattmc3@gmail.com>
2026-07-11 10:40:26 -0400

    Update fzf plugin

--- plugins/fzf/fzf.plugin.zsh
+++ plugins/fzf/fzf.plugin.zsh
@@ -24,3 +24,17 @@ fi
 
 # Use fzf for tab completions
 antibody bundle aloxaf/fzf-tab
+
+# fzf-tab ignores %F{}/%f prompt-escape sequences in completion format
+# zstyles (see fzf-tab README "Configure" section) - reset to plain text
+# so headers don't leak raw escape codes into the fzf display.
+zstyle ':completion:*:corrections' format ' -- %d (errors: %e) --'
+zstyle ':completion:*:descriptions' format ' -- %d --'
+zstyle ':completion:*:messages' format ' -- %d --'
+zstyle ':completion:*:warnings' format ' -- no matches found --'
+zstyle ':completion:*' format ' -- %d --'
+
+# _match/_approximate are redundant once fzf-tab provides interactive
+# fuzzy filtering, and their "original"/"corrections" group headers
+# get squashed together under group-name ''. Plain completion only.
+zstyle ':completion:*' completer _complete
